e11ebae2d43a858e2e99d629eb6750b471002b9c
mervekrbkmz <mervekarabekmezz@gmail.com>
2025-08-10 23:45:39 +0300

    Apı kurulumu

---
+++ Order.API/Contants/OpenTelemetryConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.API.Contants
+{
+  public class OpenTelemetryConstants
+  {
+    public string ServiceName { get; set; } = string.Empty;
+    public string ServiceVersion { get; set; } = string.Empty;
+    public string ActivitySourceName { get; set; } = string.Empty;
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Project README
+
+
+## Notes
+uygulamaın davranışını o uygulamayı kapatmadan değiştirmek istiyorsanız değişkenleri appsettingse taşımak lazım.Buraya tanımlanan değerleri memorye kaydeder. Direkt memoryden okur bir yerde hard verdiğimizde.
+
+--uygulamada program.cs 1 kere calışır. Middleware her requestle beraber calışır. Singleton ama her requestle calışır.
