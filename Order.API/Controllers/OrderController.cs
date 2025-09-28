fe868680b87d7f91e4ad18252aa5b17513723e57
Mustafa Karabekmez <karabekmezmustafa@gmail.com>
2025-09-28 19:49:29 +0300

    changes commit.

--- Order.API/Controllers/OrderController.cs
+++ Order.API/Controllers/OrderController.cs
@@ -7,5 +7,15 @@ namespace Order.API.Controllers
   [ApiController]
   public class OrderController : ControllerBase
   {
+
+    [HttpGet("get-order")]
+    public IActionResult GetOrder()
+    {
+      var c = 10;
+      var y = 0;
+      var a = c / y;
+      
+      return Ok("Order Service is working...");
+    }
   }
 }
--- Order.API/OpenTelemetry/OpenTelemetryConstants.cs
+++ Order.API/OpenTelemetry/OpenTelemetryConstants.cs
@@ -8,7 +8,7 @@ namespace Order.API.OpenTelemetry
 {
   public class OpenTelemetryConstants
   {
-    public string ServiceName { get; set; } = null!; //null olamaz.
+    public string ServiceName { get; set; } = null!;
     public string ServiceVersion { get; set; } = null!;
     public string ActivitySourceName { get; set; } =null!;
   }
